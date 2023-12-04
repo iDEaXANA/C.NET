9870b78b8b9bc76affc5192f792471b83cacb6c0
Bilal Mohamed-Benkada <bmbenkada@gmail.com>
2023-12-04 14:21:08 +0000

    Adding Hello World to pull remotely on personal

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Konichiwa, Sekai");
+// Console.WriteLine("Konichiwa, Sekai");
+// Console.Write("First");
+// Console.Write("Second");
+
+using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
