d825a2d3a8f26456b33142ae9880ec1cef2921a1
Bilal MB <bmbenkada@gmail.com>
2023-12-05 18:47:05 +0000

    all types of loops, Methods, getters and setters, Scopes and Models + troubleshooting!

--- Program.cs
+++ Program.cs
@@ -4,179 +4,324 @@
 // Console.Write("First");
 // Console.Write("Second");
 
-// using System;
 
-// namespace HelloWorld
-// {
-//     internal class Program
-//     {
-//         static void Main(string[] args)
-//         {
-//             Console.WriteLine("Hello World");
-//         }
+
+
+//// 1 byte is made up of 8 bits 00000000 - these bits can be used to store a number as follows
+// //// Each bit can be worth 0 or 1 of the value it is placed in
+// ////// From the right we start with a value of 1 and double for each digit to the left
+// //// 00000000 = 0
+// //// 00000001 = 1
+// //// 00000010 = 2
+// //// 00000011 = 3
+// //// 00000100 = 4
+// //// 00000101 = 5
+// //// 00000110 = 6
+// //// 00000111 = 7
+// //// 00001000 = 8
+
+// Byte = 8bit
+// Short = 16bit
+// Integer/float = 32bit
+// double = 64bit
+// decimal = 128bit
+
+
+//-- Below is min and max values.
+
+// 1 byte (8 bit) unsigned, where signed means it can be negative
+// byte myByte = 255;
+// byte mySecondByte = 0;
+
+// // 1 byte (8 bit) signed, where signed means it can be negative
+// sbyte mySbyte = 127;
+// sbyte mySecondSbyte = -128;
+
+
+// // 2 byte (16 bit) unsigned, where signed means it can be negative
+// ushort myUshort = 65535;
+
+// // 2 byte (16 bit) signed, where signed means it can be negative
+// short myShort = -32768;
+
+// // 4 byte (32 bit) signed, where signed means it can be negative
+// int myInt = 2147483647;
+// int mySecondInt = -2147483648;
+
+// // 8 byte (64 bit) signed, where signed means it can be negative
+// long myLong = -9223372036854775808;
+
+
+// // 4 byte (32 bit) floating point number
+// float myFloat = 0.751f;
+// float mySecondFloat = 0.75f;
+
+// // 8 byte (64 bit) floating point number
+// double myDouble = 0.751;
+// double mySecondDouble = 0.75d;
+
+// // 16 byte (128 bit) floating point number
+// decimal myDecimal = 0.751m;
+// decimal mySecondDecimal = 0.75m;
+
+// // Console.WriteLine(myFloat - mySecondFloat);
+// // Console.WriteLine(myDouble - mySecondDouble);
+// // Console.WriteLine(myDecimal - mySecondDecimal);
+
+
+
+// string myString = "Hello World";
+// // Console.WriteLine(myString);
+// string myStringWithSymbols = "!@#$@^$%%^&(&%^*__)+%^@##$!@%123589071340698ughedfaoig137";
+// // Console.WriteLine(myStringWithSymbols);
+
+// bool myBool = true;
+
+// //-- Arrays
+// string[] myGroceryArray = new string[2];
+// myGroceryArray[0] = "Avocados";
+// myGroceryArray[1] = "Ice Cream";
+// // Console.WriteLine(myGroceryArray[0]);
+// // Console.WriteLine(myGroceryArray[1]);
+// // Console.WriteLine(myGroceryArray[2]);
+
+// string[] mySecondGroceryArray = {"Blueberries", "Eggs"};
+// // Console.WriteLine(mySecondGroceryArray[0]);
+
+// //-- Lists
+// List<string> myGroceryList = new List<string>() {"Organic Milk", "Goat's Cheese"};
+// Console.WriteLine(myGroceryList[0]);
+
+// IEnumerable<string> myGroceryIEnumerable = myGroceryList;
+
+// Console.WriteLine(myGroceryIEnumerable.First());
+
+// //-- 2D Arrays
+// string[,] myTwoDimensionalArray = new string[,] {
+//     {"Blueberries", "Eggs"},
+//     {"Organic Milk", "Goat's Cheese"}
+// };
+
+// Console.WriteLine(myTwoDimensionalArray[1,1]); // Goat's Cheese
+// // For each dimension, add a comma
+
+// Dictionary<string, string> myGroceryDictionary = new Dictionary<string, string>(){
+//     {"Blueberries", "Fruit"}
+// };
+
+// Console.WriteLine(myGroceryDictionary["Blueberries"]);
+
+// // You can have that second string as a string array
+// // You have to declare the array inside the dictionary though.
+
+// Dictionary<string, string[]> myGroceryDictionary2 = new Dictionary<string, string[]>() {
+//     {"Fruit", new string[]{"Blueberries", "mangoes", "cookies"}}
+// };
+
+// Console.WriteLine(myGroceryDictionary2["Fruit"][2]);
+
+// //-- Operators 
+// int myInt = 5;
+// int mySecondInt = 10;
+
+// Console.WriteLine(Math.Pow(5,2));
+
+// string myString = "Bilal";
+
+// myString += " MB.";
+
+// Console.WriteLine(myString);
+
+// myString = myString + (" \"Hi!\"");
+
+// Console.WriteLine(myString);
+
+// string[] myStringArr = myString.Split(". ");
+
+// Console.WriteLine(myStringArr[0]);
+// Console.WriteLine(myStringArr[1]);
+
+// //-- Conditionals (operators)
+//* // same as JS.
+
+//-- Conditional Statements
+//!! if/else, switch.
+
+//* if/else, the same as JS  
+// using System.Data;
+
+// string myCow = "cows";
+//             string myCapitalisedCow = "Cow";
+//             switch (myCow) {
+//                 case "cow":
+//                     Console.WriteLine("Lowercase");
+//                     break;
+//                 case "Cow":
+//                     Console.WriteLine("Uppercase");
+//                     break;
+//                 default:
+//                     Console.WriteLine("Not Equal");
+//                     break;
+//             };
+
+//-- Loops
+//!! for, foreach, while, do while
+
+//* for,while, the same as JS  
+
+
+// int[] intsToCompress = new int[] {10, 15, 20, 25, 30, 12, 34};
+
+// // 1. add them manually
+// // 2. Use For Loop
+// DateTime startTime = DateTime.Now;
+// int totalValue = 0;
+
+// foreach(int intForCompression in intsToCompress) {
+//     if (intForCompression > 20) {
+//         totalValue += intForCompression;
 //     }
 // }
+// Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+// Console.WriteLine(totalValue);
 
 
-            //// 1 byte is made up of 8 bits 00000000 - these bits can be used to store a number as follows
-            // //// Each bit can be worth 0 or 1 of the value it is placed in
-            // ////// From the right we start with a value of 1 and double for each digit to the left
-            // //// 00000000 = 0
-            // //// 00000001 = 1
-            // //// 00000010 = 2
-            // //// 00000011 = 3
-            // //// 00000100 = 4
-            // //// 00000101 = 5
-            // //// 00000110 = 6
-            // //// 00000111 = 7
-            // //// 00001000 = 8
- 
-            // Byte = 8bit
-            // Short = 16bit
-            // Integer/float = 32bit
-            // double = 64bit
-            // decimal = 128bit
+
+// for (int i = 0; i< intsToCompress.Length; i++)
+// {
+//     totalValue += intsToCompress[i];
+// }
+// Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+// Console.WriteLine(totalValue);
+
+// startTime = DateTime.Now;
+// totalValue = 0;
+
+// foreach(int intForCompression in intsToCompress) {
+//     totalValue += intForCompression;
+// }
+
+// Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+// Console.WriteLine(totalValue);
+
+// int index = 0;
+// totalValue = 0;
+// startTime = DateTime.Now;
+
+// while(index < intsToCompress.Length) {
+//     totalValue += intsToCompress[index];
+//     index++;
+// }
+
+// Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+// Console.WriteLine(totalValue);
+
+// startTime = DateTime.Now;
+// totalValue = 0;
+// index = 0;
+// do {
+//     // Console.WriteLine(index);
+//     // Console.WriteLine(index < intsToCompress.Length)
+//     totalValue += intsToCompress[index];
+//     index++;
+// } while(index < intsToCompress.Length); 
+
+// Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+// Console.WriteLine(totalValue);
+
+// totalValue = 0;
+// totalValue = intsToCompress.Sum();
+
+// Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+// Console.WriteLine(totalValue);
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace HelloWorld
+{
+
+    public class Computer {
+        public string Motherboard {get; set;} = "";
+        // private string _motherboard;
+        // private string Motherboard {get{return _motherboard;} set(_motherboard = value;)}
+        public int CPUCores {get; set;} 
+        public bool HasWifi {get; set;}
+        public bool HasLTE {get; set;}
+        public DateTime ReleaseDate {get; set;}
+        public decimal Price {get; set;}
+        public string VideoCard {get; set;} = "";
+
+        //!! Old Way!
+        // public Computer() {
+        //     if (Motherboard == null) {
+        //         Motherboard = "";
+        //     }
+        //     if (VideoCard == null) {
+        //         VideoCard = "";
+        //     }
+        // }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //     Console.WriteLine("Hello World");
+
+        //     //-- Methods
+        //     //!! Methods, Arguments and Return.   
+        //     //* MOSTLY the same as JS  
+
+        
+        //     int totalValue = 0;
+        //     int[] intsToCompress = new int[] {10, 15, 20, 25, 30, 12, 34};
+        //     int[] intsToCompress2 = new int[] {23, 23, 53, 56, 83, 92};
+        //     DateTime startTime = DateTime.Now;
+        //     totalValue = GetSum(intsToCompress);
+        //     Console.WriteLine(totalValue);
+        //     Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+        //     totalValue = GetSum(intsToCompress2);
+        //     Console.WriteLine(totalValue);
+        //     Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+            
+        //     //-- Scope
+        //     //!! Scope.
+        //     //* MOSTLY the same as JS  
+            
+        
+        // }
+        //     static private int GetSum(int[] array) {
+            
+        //     int totalValue = 0;
+        //     foreach(int intForCompression in array) {
+        //         totalValue += intForCompression;
+        //     }
+        //     return totalValue;
+        // }
+            //-- Models (Source Mapping), Namespaces, Database Connections (Dapper, Entity Framework), Config
+            //!! Methods, Arguments and Return.   
+            //* , , the same as JS 
+
+        Computer myComputer = new Computer() {
+            Motherboard = "Z690",
+            HasWifi = true,
+            HasLTE = false,
+            ReleaseDate = DateTime.Now,
+            Price = 943.87m,
+            VideoCard = "RTX 1"
+        };
+        Console.WriteLine(myComputer.Motherboard);
+        Console.WriteLine(myComputer);
+        }
+    }
+}
+
+            
 
 
-            //-- Below is min and max values.
 
-            // 1 byte (8 bit) unsigned, where signed means it can be negative
-            // byte myByte = 255;
-            // byte mySecondByte = 0;
- 
-            // // 1 byte (8 bit) signed, where signed means it can be negative
-            // sbyte mySbyte = 127;
-            // sbyte mySecondSbyte = -128;
- 
- 
-            // // 2 byte (16 bit) unsigned, where signed means it can be negative
-            // ushort myUshort = 65535;
- 
-            // // 2 byte (16 bit) signed, where signed means it can be negative
-            // short myShort = -32768;
- 
-            // // 4 byte (32 bit) signed, where signed means it can be negative
-            // int myInt = 2147483647;
-            // int mySecondInt = -2147483648;
- 
-            // // 8 byte (64 bit) signed, where signed means it can be negative
-            // long myLong = -9223372036854775808;
- 
- 
-            // // 4 byte (32 bit) floating point number
-            // float myFloat = 0.751f;
-            // float mySecondFloat = 0.75f;
- 
-            // // 8 byte (64 bit) floating point number
-            // double myDouble = 0.751d;
-            // double mySecondDouble = 0.75d;
- 
-            // // 16 byte (128 bit) floating point number
-            // decimal myDecimal = 0.751m;
-            // decimal mySecondDecimal = 0.75m;
- 
-            // // Console.WriteLine(myFloat - mySecondFloat);
-            // // Console.WriteLine(myDouble - mySecondDouble);
-            // // Console.WriteLine(myDecimal - mySecondDecimal);
- 
- 
- 
-            // string myString = "Hello World";
-            // // Console.WriteLine(myString);
-            // string myStringWithSymbols = "!@#$@^$%%^&(&%^*__)+%^@##$!@%123589071340698ughedfaoig137";
-            // // Console.WriteLine(myStringWithSymbols);
- 
-            // bool myBool = true;
 
-            // //-- Arrays
-            // string[] myGroceryArray = new string[2];
-            // myGroceryArray[0] = "Avocados";
-            // myGroceryArray[1] = "Ice Cream";
-            // // Console.WriteLine(myGroceryArray[0]);
-            // // Console.WriteLine(myGroceryArray[1]);
-            // // Console.WriteLine(myGroceryArray[2]);
 
-            // string[] mySecondGroceryArray = {"Blueberries", "Eggs"};
-            // // Console.WriteLine(mySecondGroceryArray[0]);
-
-            // //-- Lists
-            // List<string> myGroceryList = new List<string>() {"Organic Milk", "Goat's Cheese"};
-            // Console.WriteLine(myGroceryList[0]);
-
-            // IEnumerable<string> myGroceryIEnumerable = myGroceryList;
-
-            // Console.WriteLine(myGroceryIEnumerable.First());
-
-            // //-- 2D Arrays
-            // string[,] myTwoDimensionalArray = new string[,] {
-            //     {"Blueberries", "Eggs"},
-            //     {"Organic Milk", "Goat's Cheese"}
-            // };
-
-            // Console.WriteLine(myTwoDimensionalArray[1,1]); // Goat's Cheese
-            // // For each dimension, add a comma
-
-            // Dictionary<string, string> myGroceryDictionary = new Dictionary<string, string>(){
-            //     {"Blueberries", "Fruit"}
-            // };
-
-            // Console.WriteLine(myGroceryDictionary["Blueberries"]);
-
-            // // You can have that second string as a string array
-            // // You have to declare the array inside the dictionary though.
-
-            // Dictionary<string, string[]> myGroceryDictionary2 = new Dictionary<string, string[]>() {
-            //     {"Fruit", new string[]{"Blueberries", "mangoes", "cookies"}}
-            // };
-
-            // Console.WriteLine(myGroceryDictionary2["Fruit"][2]);
-
-            // //-- Operators 
-            // int myInt = 5;
-            // int mySecondInt = 10;
-
-            // Console.WriteLine(Math.Pow(5,2));
-
-            // string myString = "Bilal";
-
-            // myString += " MB.";
-
-            // Console.WriteLine(myString);
-
-            // myString = myString + (" \"Hi!\"");
-
-            // Console.WriteLine(myString);
-
-            // string[] myStringArr = myString.Split(". ");
-
-            // Console.WriteLine(myStringArr[0]);
-            // Console.WriteLine(myStringArr[1]);
-
-            // //-- Conditionals (operators)
-            //* // same as JS.
-
-            //-- Conditional Statements
-            //!! if/else, switch.
-
-            //* if/else, the same as JS  
-            string myCow = "cows";
-            string myCapitalisedCow = "Cow";
-            switch (myCow) {
-                case "cow":
-                    Console.WriteLine("Lowercase");
-                    break;
-                case "Cow":
-                    Console.WriteLine("Uppercase");
-                    break;
-                default:
-                    Console.WriteLine("Not Equal");
-                    break;
-            };
-
-            //-- Loops
-            //!! for, while, do while, Methods, Arguments, Return and Scope.
-
-            //* , the same as JS  
 
             
         
--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8dc79978297d2b4964ff2863fd0416a128d0cac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea22f7ffd72cb03ed7a131f932f01fc240f614a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
