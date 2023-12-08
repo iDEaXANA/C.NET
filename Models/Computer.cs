86b65fe747784e6b4889203e14cff3a09af4de46
Bilal MB <bmbenkada@gmail.com>
2023-12-08 16:44:29 +0000

    Forgot -am flag doesn't apply to untracked files :(((

---
+++ Models/Computer.cs
@@ -0,0 +1,25 @@
+namespace CNET.C.NET.Models {
+
+    public class Computer {
+        public string Motherboard {get; set;} = "";
+        // private string _motherboard;
+        // private string Motherboard {get{return _motherboard;} set(_motherboard = value;)}
+        public int CPUCores {get; set;} 
+        public bool HasWifi {get; set;}
+        public bool HasLTE {get; set;}
+        public DateTime ReleaseDate {get; set;}
+        public decimal Price {get; set;}
+        public string VideoCard {get; set;} = "";
+
+        //!! Old Way!
+        // public Computer() {
+        //     if (Motherboard == null) {
+        //         Motherboard = "";
+        //     }
+        //     if (VideoCard == null) {
+        //         VideoCard = "";
+        //     }
+        // }
+    }
+    
+}
